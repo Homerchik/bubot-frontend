73e8cba3d2a31af79d568d84d72b0a812dc5c2bd
Yevhenii.Hulkov <Yevhenii.Hulkov@acronis.com>
2023-11-14 16:41:20 +0100

    experiments

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup>
   import { ref } from 'vue'
-  // import { WebApp } from 'tg-lib'
 
   const count = ref(0)
   const vals = new Array(1,2,3,4)
-  const TWebApp = this.TWA
-
 
 </script>
 
 <template>
   <main>
-    <h1>{{ TWebApp.query_id }}</h1>
+    <h1>{{ TWA.query_id }}</h1>
     <button @click="count++">You clicked me {{  count }} times.</button>
     <p><select>
       <option v-for="i in vals" :key="i" :value="i">{{ i + " value " + count}}</option>
