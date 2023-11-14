34850a8c553a9b1f2a7a725f7b40b5930494910b
Yevhenii.Hulkov <Yevhenii.Hulkov@acronis.com>
2023-11-14 16:38:17 +0100

    experiments

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,14 @@
 
   const count = ref(0)
   const vals = new Array(1,2,3,4)
+  const TWebApp = this.TWA
 
 
 </script>
 
 <template>
   <main>
-    <h1>{{ this.TWA.query_id }}</h1>
+    <h1>{{ TWebApp.query_id }}</h1>
     <button @click="count++">You clicked me {{  count }} times.</button>
     <p><select>
       <option v-for="i in vals" :key="i" :value="i">{{ i + " value " + count}}</option>
